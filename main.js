e14c8190319eeec813276477d081c346491f751f
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-06-28 19:46:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
+noseX = 0;
+noseY = 0;
+
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
-    video.size(500,400)
-    video.position(250,190);
+    video.size(500,400);
 
-    canvas = createCanvas(500,380);
-    canvas.center();
-
-    poseNet = ml5.poseNet(video, function(){
-        console.log("Model Loaded");
-    })
+    canvas = createCanvas(500,400);
+    canvas.position();
     
-    poseNet.on("pose", gotPoses);
-}
+    poseNet = ml5.poseNet(video,function(){
+        console.log("Model Loaded");
+    });
+    
+    poseNet.on("pose",function(results){
+        console.log("Got Results");
 
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results)
-    }
+        if (results.length > 0){
+            console.log(results);
+
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("Nose X = " + noseX, "Nose Y = " + noseY);
+
+            rightWristX = results[0].pose.rightWrist.x;
+            leftWristX = results[0].pose.leftWrist.x;
+            console.log("Right Wrist X = " + rightWristX, "Left Wrist X = " + leftWristX);
+
+            difference = floor(leftWristX - rightWristX);
+            console.log("Difference = " + difference);
+        }
+    })
 }
 
 function draw(){
     background("red");
+
+    fill("aqua");
+    stroke("yellow");
+    textSize(difference);
+    text("Sreeyash",50,400);
+
+    document.getElementById("squareSize").innerHTML = "The Size Of The Square Is Aproximatly " + difference + " px";
 }
